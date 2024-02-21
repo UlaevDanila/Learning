7ab78607038f68f210eb067a543309d223f25b88
Immortal_342 <ulaev03@mail.ru>
2024-02-21 19:08:56 +0300

    Added prefabs folder and player prefab

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -1,64 +1,61 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace Movement
 {
     public class PlayerMovement : MonoBehaviour
-    {
-    [SerializeField] private float _jumpVelocity;
-
-    [SerializeField] private float _movementVelocity;
-
-    private StateMachine _stateMachine;
-    
-    private Rigidbody2D _rigidbody2D;
-
-    private SpriteRenderer _spriteRenderer;
-
-    private CollisionsHandler _collisionsHandler;
-
-    private InputManager _inputManager;
-
-    private float _previousYPosition;
-
-    private void Awake()
-    {
-        _collisionsHandler = GetComponent<CollisionsHandler>();
-        _inputManager = GetComponent<InputManager>();
-        _rigidbody2D = GetComponent<Rigidbody2D>();
-        _stateMachine = GetComponent<StateMachine>();
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-    }
-
-    private void Update()
-    {
-        if (_inputManager.InputDirection < 0)
-            _spriteRenderer.flipX = true;
-        if(_inputManager.InputDirection > 0 && _spriteRenderer.flipX)
-            _spriteRenderer.flipX = false;
-
-        if (_collisionsHandler.IsOnAir && (transform.position.y > _previousYPosition))
         {
-            _stateMachine.SetState("isRunning", "isJumping");
-        }
-        else if(_collisionsHandler.IsOnAir && (transform.position.y < _previousYPosition))
-        {
-            _stateMachine.SetState("isJumping", "isFalling");
-        }
-    }
+        [SerializeField] private float jumpVelocity;
 
-    private void LateUpdate()
-    {
-        _previousYPosition = transform.position.y;
-    }
+        [SerializeField] private float movementVelocity;
 
-    private void FixedUpdate()
-    {
-        float velocityMultiplication = _movementVelocity*Time.fixedDeltaTime;
-        float verticalVelocity = _inputManager.JumpButtonPressed && !_collisionsHandler.IsOnAir ? _jumpVelocity : _rigidbody2D.velocity.y;
-        _rigidbody2D.velocity = new Vector2(_inputManager.InputDirection * velocityMultiplication, verticalVelocity);
+        private StateMachine _stateMachine;
         
-    }
-    
+        private Rigidbody2D _rigidbody2D;
+
+        private SpriteRenderer _spriteRenderer;
+
+        private CollisionsHandler _collisionsHandler;
+
+        private InputManager _inputManager;
+
+        private float _previousYPosition;
+
+        private void Awake()
+        {
+            _collisionsHandler = GetComponent<CollisionsHandler>();
+            _inputManager = GetComponent<InputManager>();
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+            _stateMachine = GetComponent<StateMachine>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void Update()
+        {
+            if (_inputManager.InputDirection < 0)
+                _spriteRenderer.flipX = true;
+            if(_inputManager.InputDirection > 0 && _spriteRenderer.flipX)
+                _spriteRenderer.flipX = false;
+
+            if (_collisionsHandler.IsOnAir && (transform.position.y > _previousYPosition))
+            {
+                _stateMachine.SetState("isRunning", "isJumping");
+            }
+            else if(_collisionsHandler.IsOnAir && (transform.position.y < _previousYPosition))
+            {
+                _stateMachine.SetState("isJumping", "isFalling");
+            }
+        }
+
+        private void LateUpdate()
+        {
+            _previousYPosition = transform.position.y;
+        }
+
+        private void FixedUpdate()
+        {
+            float velocityMultiplication = movementVelocity*Time.fixedDeltaTime;
+            float verticalVelocity = _inputManager.JumpButtonPressed && !_collisionsHandler.IsOnAir ? jumpVelocity : _rigidbody2D.velocity.y;
+            _rigidbody2D.velocity = new Vector2(_inputManager.InputDirection * velocityMultiplication, verticalVelocity);
+        }
     }
 }
