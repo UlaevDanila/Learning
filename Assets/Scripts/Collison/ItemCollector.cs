37ab378f003e144b955364109ab75dc122f9914f
Immortal_342 <ulaev03@mail.ru>
2024-02-26 14:13:28 +0300

    Added traps and first UI elements

--- Assets/Scripts/Collison/ItemCollector.cs
+++ Assets/Scripts/Collison/ItemCollector.cs
@@ -1,16 +1,18 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography;
+using TMPro;
 using UnityEngine;
 
 public class ItemCollector : MonoBehaviour
 {
+    private int _cherriesCounter = 0;
+
+    [SerializeField] private TextMeshProUGUI cherriesText;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Cherry"))
         {
             Destroy(other.gameObject);
+            _cherriesCounter += 1;
+            cherriesText.text = "Cherries: " + _cherriesCounter; 
         }
     }
 }
